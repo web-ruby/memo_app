92fe2c7069dffee362fe489b7670ac1666132e3e
web-ruby <outstandinglife718@gmail.com>
2020-04-21 21:37:29 +0900

    Designed Memo class

--- app.rb
+++ app.rb
@@ -5,106 +5,95 @@ require 'sinatra/reloader'
 require 'json'
 require 'byebug'
 
-def json_file
-  open('views/memos.json') do |io|
-    JSON.load(io)
-  end
-end
-
-def memo(memo_id)
-  w_memo = ''
-  json_file['memos'].each do |memo|
-    w_memo = memo if memo['id'].to_s == memo_id.to_s
-  end
-  w_memo
-end
-
-def new_memo
-  add_memo
-  json = json_file
-  json['memos'].push(add_memo)
-  File.open('views/memos.json', 'w') { |file| JSON.dump(json, file) }
-end
-
-def add_memo
-  new_id = 0
-  json = json_file
-  json['memos'].each do |memo|
-    new_id = memo['id'].to_i + 1 if new_id <= memo['id'].to_i
-  end
-  {
-    'id' => new_id.to_s,
-    'title' => params[:title],
-    'body' => params[:body]
-  }
-end
-
-def edit_memo
-  {
-    'id' => params[:id].to_s,
-    'title' => params[:title],
-    'body' => params[:body]
-  }
-end
-
-def delete_memo
-  num = 0
-  json = json_file
-  json['memos'].each do |memo|
-    json['memos'].delete_at(num) if memo['id'].to_s == params[:id].to_s
-    num += 1
-  end
-  File.open('views/memos.json', 'w') { |file| JSON.dump(json, file) }
-end
-
-def rewrite_memo(edit_memo)
-  num = 0
-  json = json_file
-  json['memos'].each do |memo|
-    if memo['id'].to_s == params[:id].to_s
-      json['memos'][num]['title'] = edit_memo['title']
-      json['memos'][num]['body'] = edit_memo['body']
+class Memo
+  def self.json_file
+    open('views/memos.json') do |io|
+      JSON.load(io)
     end
-    num += 1
   end
-  File.open('views/memos.json', 'w') { |file| JSON.dump(json, file) }
+
+  def self.memo(id)
+    w_memo = ''
+    json_file['memos'].each do |memo|
+      w_memo = memo if memo['id'].to_s == id.to_s
+    end
+    w_memo
+  end
+
+  def self.new(title, body)
+    new_id = 0
+    json = json_file
+    json['memos'].each do |memo|
+      new_id = memo['id'].to_i + 1 if new_id <= memo['id'].to_i
+    end
+    add = {
+      'id' => new_id.to_s,
+      'title' => title,
+      'body' => body
+    }
+    json = json_file
+    json['memos'].push(add)
+    File.open('views/memos.json', 'w') { |file| JSON.dump(json, file) }
+  end
+
+  def self.delete(id)
+    num = 0
+    json = json_file
+    json['memos'].each do |memo|
+      json['memos'].delete_at(num) if memo['id'].to_s == id.to_s
+      num += 1
+    end
+    File.open('views/memos.json', 'w') { |file| JSON.dump(json, file) }
+  end
+
+  def self.rewrite(id, title, body)
+    num = 0
+    json = json_file
+    json['memos'].each do |memo|
+      if memo['id'].to_s == id
+        json['memos'][num]['title'] = title
+        json['memos'][num]['body'] = body
+      end
+      num += 1
+    end
+    File.open('views/memos.json', 'w') { |file| JSON.dump(json, file) }
+  end
 end
 
 get '/' do
-  @memos = json_file['memos']
+  @memos = Memo.json_file['memos']
   erb :index
 end
 
 get '/memo/:id' do
-  @memo = memo(params[:id])
+  @memo = Memo.memo(params[:id])
   erb :show_memo
 end
 
 get '/memo/edit/:id' do
-  @memo = memo(params[:id])
+  @memo = Memo.memo(params[:id])
   erb :edit
 end
 
 patch '/memo/edit/:id' do
-  edit_memo
-  rewrite_memo(edit_memo)
+  Memo.rewrite(params[:id], params[:title], params[:body])
   redirect '/'
   erb :index
 end
 
 delete '/memo/delete/:id' do
-  delete_memo
+  Memo.delete(params[:id])
   redirect '/'
   erb :index
 end
 
 get '/create' do
-  @memos = json_file['memos']
+  @memos = Memo.json_file['memos']
   erb :create
 end
 
 post '/new' do
-  new_memo
+  Memo.new(params[:title], params[:body])
   redirect '/'
   erb :index
 end
